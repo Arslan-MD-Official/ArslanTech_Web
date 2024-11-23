ccf81c0b553a1d758a38293a1a6da29577e92adc
ArslanMD <arslanofficialmd@gmail.com>
2024-11-23 12:56:26 +0500

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'tohal55486@unobitex.com',
-    password: 'Tohal55486@',
+    email: 'gamaf57278@walshun.com',
+    password: 'Gamaf57278@@',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
